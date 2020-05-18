176753512691c2816b21563e00bf1510e7cde2f7
Stiven López Giraldo <stiven.lopez@datalytics.com>
2020-05-17 22:59:52 -0500

    se crean las funciones para hacer la inferencia

---
+++ predict.py
@@ -0,0 +1,28 @@
+import joblib
+import pandas as pd
+
+
+def load_model(model_path: str):
+    """
+    load serialize model
+    :param model_path:
+    :return:
+    """
+    model = joblib.load(filename=model_path)
+    return model
+
+
+def predict(model, data):
+    """
+
+    :param model:
+    :param data:
+    :return:
+    """
+    data = pd.DataFrame.from_dict(data)
+    prediction = {
+        'prediction': model.predict(data)
+    }
+    return prediction
+
+
